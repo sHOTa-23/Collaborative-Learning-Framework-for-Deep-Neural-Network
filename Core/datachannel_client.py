finally setted up the averaging logic, fixed many issues

Core/datachannel_client.py
@@ -23,23 +23,21 @@ class DatachannelClient():
         logging.debug("Datachannel client initialized")
 
     def start(self):
+        logging.debug("Datachannel client started")
         with open(self.id_path) as f:
             self.id = f.read()
         f.close()
         self.connect_server()
-
         self.load_model()
         self.load_input()
         self.calculate_new_waits()
-        print(self.model.trainable_variables)
         self.send_model(self.model)
         
     def connect_server(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.connect((self.ip, self.port))
-        logging.debug("Connected to server")
+        logging.debug("Connected to server from {}".format(self.server.getsockname()))
         # Waiting for server to get permission to continue
-        self.server.recv(1024)
 
     def calculate_new_waits(self):
         if self.model_type == "pytorch":
@@ -80,13 +78,35 @@ class DatachannelClient():
             self.model = torch.jit.load(self.model_path)
         logging.info(f"{self.model_type} Model loaded")     
 
+    def set_controller(self,controller):
+        self.controller = controller
+    
+
     def send_model(self,array):
-        data = prepare_model(array)
-        print('Sent {}'.format(self.id))
+        message = self.server.recv(1024).decode()
+        if message == 'start':
+            logging.debug("Server ready to receive model")
+        else :
+            logging.debug("Server did not receive start message instead received: " + message)
+        
+        logging.info('client Sent ID: {}'.format(self.id))
         self.server.sendall(self.id.encode())
+        status = self.server.recv(1024).decode()
+        if status == 'Id Verified':
+            logging.debug("Server received ID")
+        data = prepare_model(array)
+       
         self.server.sendall(data)
         self.server.sendall(b'EOF')
         logging.info("Model sent by Client")
-        self.model = receive(self.server)
-        logging.info("Model Received by Client")
-        print(self.model.trainable_variables)
+        message = self.server.recv(1024).decode()
+        if message == 'calculation completed':
+            logging.debug("Server ready to receive new model")
+            logging.info(str(self.controller.ping_client.get_status()) + " Before")
+            self.controller.fire_ping()
+            logging.info(str(self.controller.ping_client.get_status()) + " After")
+        else:
+            logging.debug("Server did not receive calculation completed message instead received: " + message)
+        # self.model = receive(self.server)
+        # print(list(self.model.parameters()))
+        # logging.info("Model Received by Client")

Server/datachannel_server.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.NOTSET)
 
 
 class DatachannelServer:
-    def __init__(self, ip, port,clientsDB, listener_num = 100, gap_time=15):
+    def __init__(self, ip, port,clientsDB, listener_num = 100, gap_time=20):
         self.ip = ip
         self.port = port
         self.listener_num = listener_num
@@ -20,8 +20,6 @@ class DatachannelServer:
         self.server = None
         logging.debug('Datachannel initialized')
     
-    def set_controller(self, controller):
-        self.controller = controller
         
     def start(self):
         if self.server is not None and self.server.fileno() != -1:
@@ -48,28 +46,57 @@ class DatachannelServer:
         main_thread.start()
         logging.debug('main_thread started')
 
+    def calculate_average(self):
+        import torch
+        keys = list(self.received_values.keys())
+        print("SDASDASDASDASD",self.received_values)
+        if len(keys) == 0:
+            return None
+        init_weights = self.received_values[keys[0]]
+       
+        print("Before")
+        for key in keys:
+            for i in self.received_values[key].parameters():
+                print(i)
+            print("Another Model")
+        for mdl in range(1,len(keys)):
+            with torch.no_grad():
+                for (i,j) in zip(init_weights.parameters(),self.received_values[keys[mdl]].parameters()):
+                    i+=j 
+        
+        print("After")
+        for i in init_weights.parameters():
+            print(i)
+        with torch.no_grad():
+            for i in init_weights.parameters():
+                i/=len(keys)
+        
+        return init_weights
+        
+    
     def count_average(self):
         try:
             self.barrier.wait()
         except:
             logging.warning('In count_average, barrier broke')
             pass
-        print("REeEccc ", self.received_values)
-        for client_socket in self.received_values:
-            model = self.received_values[client_socket]
-            print("Model received: {}".format(model.trainable_variables))
-            data = prepare_model(self.received_values[client_socket])
-            client_socket.sendall(data)
-            client_socket.sendall(b'EOF')
-            logging.info('Data sent to client {}'.format(client_socket.getpeername()))
+        averaged_model = self.calculate_average()
+        
+        # for client_socket in self.received_values:
+        #     data = prepare_model(averaged_model)
+        #     client_socket.sendall(data)
+        #     client_socket.sendall(b'EOF')
+        #     logging.info('Data sent to client {}'.format(client_socket.getpeername()))
         
         # Interrupt server.accept() with fake connection
+        
         fake_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         IP_address = self.ip
         Port = self.port
         fake_server.connect((IP_address, Port))
+        for client_socket in self.received_values:
+            client_socket.send(b'calculation completed')
         logging.debug("Poison packet has been sent to the DataChannel server")
-        self.controller.fire_ping()
 
     def check_time(self):
         while True:
@@ -82,22 +109,23 @@ class DatachannelServer:
                 break
 
     def client_handler(self, client_socket):
-        client_socket.send(b'Giving permission to start')
-        client = client_socket.recv(1024).decode()
+        client_socket.send(b'start')
+        client = client_socket.recv(1024)
+        client = client.decode('utf-8')
+        client_socket.send(b'Id Verified')
         logging.info("ID has been received in datachannel server by {}".format(client_socket.getpeername()))
         if self.barrier.broken:
             client_socket.send(b'I won\'t receive connections anymore!')
             return
+        # message = int(client_socket.recv(1024).decode())
         model = receive(client_socket)
         logging.info("Model has been received in datachannel server by {}".format(client_socket.getpeername()))
-        print(model.trainable_variables)
         if client not in self.clientsDB.get_clients():
             client_socket.send(b'I don\'t know you!')
             print("I don't know you!")
             return
         self.last_time = datetime.datetime.now()
         self.received_values[client_socket] = model
-        print("AAAAABa: ", self.received_values)
 
     def runner_thread(self):
         while True:

t1.py
@@ -1,6 +1,4 @@
-# # import torch
-# # import torch.nn as nn
-# # import torch.nn.functional as F
+
 
 # # # class Net(nn.Module):
 
@@ -26,28 +24,52 @@
 # # #         x = self.fc3(x)
 # # #         return x
 
-# # model = nn.Sequential(nn.Linear(2, 3),
-# #                       nn.ReLU(),
-# #                       nn.Linear(3, 1),
-# #                       nn.Sigmoid())
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# import itertools 
+# a = torch.jit.load('models/torch.pt')
+# b = torch.jit.load('models/torch.pt')
+# print(list(a.parameters()))
+# with torch.no_grad():
+#     for (i,j) in zip(a.parameters(),b.parameters()):
+#     # print(i)
+#     # print("______")
+#     # print(j)
+#     # print("++++++")
+#     # print(i+j)
+#     # print("====")
+#         i+=j 
+# for (i,j) in zip(a.parameters(),b.parameters()):
+#     print(i)
+#     print("######")
+#     print(j)
+#     print("######")
+    
+
+    # [tensor([[ 0.4771,  0.2509],
+    #     [-0.0405, -0.4350],
+    #     [-0.4727, -0.2470]], requires_grad=True), tensor([-0.6241, -0.2363,  0.5567], requires_grad=True), tensor([[-0.3088, -0.4424
 
 
-# # # MSE_loss_fn = nn.MSELoss()
-# # # from t2 import A
-# # # a = A(torch.Tensor([[1,2,3],[4,5,6]]),torch.Tensor([[1,2,3],[4,5,6]]),MSE_loss_fn)
-# # # print(a.bla())
-# # input = torch.randn(2)
-# # output = torch.Tensor([0.2131231])
 
-# # import pickle 
-# # pickle.dump(input, open("input.pkl", "wb"))
-# # pickle.dump(output, open("output.pkl", "wb"))
+model = nn.Sequential(nn.Linear(2, 3),
+                      nn.ReLU(),
+                      nn.Linear(3, 1),
+                      nn.Sigmoid())
+m = torch.jit.script(model)
+torch.jit.save(m, 'torch1.pt')
+input = torch.randn(2)
+output = torch.Tensor([0.123123])
+
+import pickle 
+pickle.dump(input, open("torchinp1.pkl", "wb"))
+pickle.dump(output, open("torchout1.pkl", "wb"))
 # # # print(output)
 # # # print(output1)
 # # # print(input)
-# # m = torch.jit.script(model)
 
-# # torch.jit.save(m, '1.pt')
 
 # # # import tensorflow as tf
 # # # import numpy as np
@@ -166,10 +188,10 @@
 
 # import torch
 
-import socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(("127.0.0.1", 9076))
-server.close()
-print(server)       
-print(server.fileno())
+# import socket
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# server.bind(("127.0.0.1", 9076))
+# server.close()
+# print(server)       
+# print(server.fileno())

test_clients.py
@@ -1,10 +1,12 @@
 from Core.app_clients import AppClient
 import torch.nn as nn
-import tensorflow as tf
-tf.autograph.set_verbosity(1)
-tf.get_logger().setLevel('INFO')
-optimizer = tf.keras.optimizers.SGD(learning_rate=1.7)
-#Tensorflow mse loss function
-loss = tf.keras.losses.MSE
-app = AppClient('Core/conf.yml',loss,optimizer)
+# import tensorflow as tf
+# tf.autograph.set_verbosity(1)
+# tf.get_logger().setLevel('INFO')
+# optimizer = tf.keras.optimizers.SGD(learning_rate=1.7)
+# #Tensorflow mse loss function
+# loss = tf.keras.losses.MSE
+loss = nn.MSELoss()
+app = AppClient('Core/conf.yml',loss)
 app.run()
+