71b394a4e8a346e37c7d718d8670ca0f2892798f
oemnadze <oemnadze@gmail.com>
2022-09-05 23:02:26 +0400

    Average tensorflow tests done

--- Test/test_datachannel_count_average.py
+++ Test/test_datachannel_count_average.py
@@ -5,22 +5,32 @@ from Server.ClientsRepository import ClientsRepository
 from Client.utils import load_model
 import torch
 import yaml
+import tensorflow as tf
 from test_utils import *
 
 conf = None
 with open('server_conf.yml') as f:
     conf = yaml.safe_load(f)
 
-def score_fn(pred,golden):
+def score_fn_pytorch(pred,golden):
     ans_index = golden.argmax(dim=1)
     score = (pred.argmax(dim=1) == ans_index).sum().item()/ans_index.shape[0]
     if score == 0:
         return 0.5
     return score
 
+def score_fn_tensorflow(pred,golden):
+    b = tf.math.argmax(input = golden)
+    b1 = tf.math.argmax(input = pred)
+    c = tf.keras.backend.eval(b==b1)
+    score = sum(c)/len(c)
+    if score == 0:
+        return 0.5
+    return score
+
 def test_count_average_pytorch():
     res = True
-    datachannel_server = DatachannelServer('127.0.0.1', 9961, ClientsRepository(conf["mongodb_host"]), "", "pytorch", "goldeninp.pkl", "goldenout.pkl", score_fn)
+    datachannel_server = DatachannelServer('127.0.0.1', 9961, ClientsRepository(conf["mongodb_host"]), "", "pytorch", "goldeninp.pkl", "goldenout.pkl", score_fn_pytorch)
     model1 = load_model("pytorch", "pytorch_models_for_average/model1.pt")
     model2 = load_model("pytorch", "pytorch_models_for_average/model1.pt")
     with torch.no_grad():
@@ -40,7 +50,7 @@ def test_count_average_pytorch():
 
 def test_count_average_tensorflow():
     res = True
-    datachannel_server = DatachannelServer('127.0.0.1', 9961, ClientsRepository(conf["mongodb_host"]), "", "tensorflow", "goldeninp.pkl", "goldenout.pkl", score_fn)
+    datachannel_server = DatachannelServer('127.0.0.1', 9961, ClientsRepository(conf["mongodb_host"]), "", "tensorflow", "tfinput.pkl", "tfoutput.pkl", score_fn_tensorflow)
     model1 = load_model("tensorflow", "pytorch_models_for_average/tf.h5")
     model2 = load_model("tensorflow", "pytorch_models_for_average/tf.h5")
     for layer in model1.layers:
@@ -66,5 +76,4 @@ def test_count_average_tensorflow():
     res = res and assert_equals(model3, averaged_model, "Model averaged incorrectly!!")
     return res
 
-# final_score(test_count_average_pytorch)
-final_score(test_count_average_tensorflow)
+final_score(test_count_average_pytorch, test_count_average_tensorflow)
