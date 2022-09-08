8b70b6a9258e24a7d9b437cf2a49f7d6c083af26
oemnadze <oemnadze@gmail.com>
2022-09-08 23:42:33 +0400

    bat fixed

--- Test/test.bat
+++ Test/test.bat
@@ -1,4 +1,4 @@
-set tests=test_server_utils.py test_server.py test_versioning.py test_datahannel_count_average.py test_client.py test_datachannel_client.py
+set tests=test_server_utils.py test_server.py test_versioning.py test_datachannel_count_average.py test_client.py test_datachannel_client.py
 for %%f in (
         %tests%
         ) do (
